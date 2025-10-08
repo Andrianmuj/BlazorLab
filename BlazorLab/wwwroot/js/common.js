70213197cb03714c8ccb5b3b386c224281f707a8
Andrian <andriangultiano14@gmail.com>
2025-10-08 22:45:33 +0800

    ToastTr with little configuration

---
+++ BlazorLab/wwwroot/js/common.js
@@ -0,0 +1,12 @@
+﻿window.showToastr = function (type, message) { 
+
+if (type == "success") {
+    toastr.success(message);
+} else if (type == "error") {
+    toastr.error(message);
+} else if (type == "info") {
+    toastr.info(message);
+} else if (type == "warning") {
+    toastr.warning(message);
+}
+}
