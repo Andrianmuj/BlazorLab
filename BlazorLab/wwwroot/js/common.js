1d539f80aaf1b4ab8f3a19d126a93bb6972b9f3b
Andrian <andriangultiano14@gmail.com>
2025-10-09 23:25:57 +0800

    Sweat Alert

--- BlazorLab/wwwroot/js/common.js
+++ BlazorLab/wwwroot/js/common.js
@@ -10,3 +10,50 @@ if (type == "success") {
     toastr.warning(message);
 }
 }
+
+window.ShowSwal = function (type, message) {
+    
+    if (type == "success")
+    {
+        Swal.fire({
+            title: "Good job!",
+            text: message,
+            icon: "success"
+        });
+    }
+
+    if (type == "error") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: message,
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
+    }
+
+    if (type == "info") {
+        Swal.fire({
+            title: message,
+            icon: "success",
+            draggable: true
+        });
+    }
+
+    if (type == "warning") {
+        Swal.fire({
+            title: message,
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: "Save",
+            denyButtonText: `Don't save`
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                Swal.fire("Saved!", "", "success");
+            } else if (result.isDenied) {
+                Swal.fire("Changes are not saved", "", "info");
+            }
+        });
+    }
+
+}
